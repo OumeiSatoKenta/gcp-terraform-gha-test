f3fe4eed49c9805660d5ac33acfa4388ac23b35c
さとうけんた <satoken@YUKAnoMac-mini.local>
2025-11-11 19:18:47 +0900

    test

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,11 @@
 # VPC ネットワーク
-resource "google_compute_network" "vpc" {
-  name                            = "terraform-test"
-  description                     = "VPC network"
-  auto_create_subnetworks         = false
-  delete_default_routes_on_create = false
-  mtu                             = 1460
-  routing_mode                    = "REGIONAL"
-
-  project = var.project_id
-}
+#resource "google_compute_network" "vpc" {
+#  name                            = "terraform-test"
+#  description                     = "VPC network"
+#  auto_create_subnetworks         = false
+#  delete_default_routes_on_create = false
+#  mtu                             = 1460
+#  routing_mode                    = "REGIONAL"
+#
+#  project = var.project_id
+#}
